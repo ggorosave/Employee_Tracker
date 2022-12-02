2da527183d7cf35f162a54e42d6cc6928373b4eb
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-02 16:58:20 -0700

    Added comment

--- db/query.sql
+++ db/query.sql
@@ -4,3 +4,5 @@ SELECT e.first_name AS 'First Name', e.last_name AS 'Last Name', r.title AS Titl
 FROM employee e
 JOIN role r ON e.role_id = r.id
 JOIN department d ON r.department_id = d.id
+
+-- Figure out GROUP BY
